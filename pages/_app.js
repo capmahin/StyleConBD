72504a5923914fcf7e00c16fef5ab1f32a7bed0a
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-30 05:56:09 +0600

    add header and footer components in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import Head from "next/head"
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (

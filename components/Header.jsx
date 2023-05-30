348fc2e80ad7fcd681b321ab89d222802d5da0be
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-31 03:43:14 +0600

    add more classes

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,12 +25,22 @@ const Header = () => {
         <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
 
         <div className="flex items-center gap-2 text-black">
+          {/* icon start */}
              <div className="w-8 md:w-12 h-8 md:h-12 rounded-full justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-              <BsCart className="text=[15px]"/>
-              <div>5</div>
+              <IoMdHeartEmpty className="text-[15px] md:text-[20px]"/>
+              <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">5</div>
 
              </div>
+             {/* icon end */}
+          {/* icon start */}
+             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text-[15px] md:text-[20px]"/>
+              <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">5</div>
+
+             </div>
+             {/* icon end */}
         </div>
+        
       </Wrapper>
 
     </header>

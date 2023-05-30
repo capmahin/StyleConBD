89cc8cb18cee81a5a972d64810ce534214299d77
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-31 03:00:18 +0600

    add more class

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,11 +18,19 @@ const Header = () => {
   return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 top-0 transition-transform duration-300 ${show}`}>
       
-      <Wrapper className="h-[60px] flex justify-between items-center">
+      <Wrapper className="h-[60px] flex justify-between items-center ">
         <Link href="/">
         <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
         </Link>
         <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+
+        <div className="flex items-center gap-2 text-black">
+             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text=[15px]"/>
+              <div>5</div>
+
+             </div>
+        </div>
       </Wrapper>
 
     </header>

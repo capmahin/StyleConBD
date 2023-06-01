a394313e6b9a8b9130141c9ca62f201c1492ca25
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-02 04:13:43 +0600

    add new

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,12 +17,28 @@ const Header = () => {
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
 
+
+  // create controlNavbar method
+
+  const controlNavbar = ()=>{
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+          setShow("-translate-y-[80px]");
+      } else {
+          setShow("shadow-sm");
+      }
+  } else {
+      setShow("translate-y-0");
+  }
+  setLastScrollY(window.scrollY);
+  }
+
   useEffect(()=>{
     window.addEventListener("scroll", controlNavbar);
     return () =>{
        window.removeEventListener("scroll",controlNavbar)
     }
-  })
+  }, [lastScrollY])
 
   return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 top-0 transition-transform duration-300 ${show}`}>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="h-[2000px]">
       Home Page ....
     </main>
   )

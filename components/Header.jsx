249e0b819aa3f7df04ccfc469e0c34549f077c36
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-02 03:29:09 +0600

    add useEffect

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,6 +16,11 @@ const Header = () => {
   const [showCatMenu, setShowCatMenu] = useState(false);
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(()=>{
+    window.addEventListener("scroll", controlNavbar);
+  })
+
   return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 top-0 transition-transform duration-300 ${show}`}>
       

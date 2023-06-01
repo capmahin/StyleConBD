8f093dd44edb9a18cd893d00fd4ed5decc728bb9
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-02 03:25:49 +0600

    add new

--- components/Header.jsx
+++ components/Header.jsx
@@ -31,14 +31,14 @@ const Header = () => {
 
         <div className="flex items-center gap-2 text-black">
           {/* icon start */}
-             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
               <IoMdHeartEmpty className="text-[19px] md:text-[24px]"/>
-              <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">51</div>
+              <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full  bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">51</div>
 
              </div>
              {/* icon end */}
           {/* icon start */}
-             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
               <BsCart className="text-[15px] md:text-[20px]"/>
               <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">5</div>
 
@@ -46,7 +46,7 @@ const Header = () => {
              {/* icon end */}
 
              {/* mobile icon start */}
-             <div  className="w-8 md:w-12 h-8 md:h-12 rounded-full justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+             <div  className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
               {
                 mobileMenu ? (
                       <VscChromeClose className="text-[16px]"

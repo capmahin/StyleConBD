1d61fa0aac54de67c8abe938c8beb5765f4b6722
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-02 20:14:52 +0600

    add carousel

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
+import { BiArrowBack } from "react-icons/bi";
+
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className="relative text-white text-[20px] w-full max-w-[1360px] mx-auto">
+        <Carousel>
+            
+        </Carousel>
+    </div>
   )
 }
 

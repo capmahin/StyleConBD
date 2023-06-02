e1b3d6b3ee71a9bad70d38f731e3929d860efd64
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-02 20:33:05 +0600

    add shop now button

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -9,17 +9,12 @@ const HeroBanner = () => {
     <div className="relative text-white text-[20px] w-full max-w-[1360px] mx-auto">
         <Carousel>
                 <div>
-                    <img src="/slide-1.png" />
-                    <p className="legend">Legend 1</p>
+                    <img src="/slide-1.png" className="aspect-[16/10] md:aspect-auto object-cover" />
+                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+                        Shop now
+                    </div>
                 </div>
-                <div>
-                    <img src="/slide-2.png" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="/slide-3.png" />
-                    <p className="legend">Legend 3</p>
-                </div>
+                
             </Carousel>
     </div>
   )

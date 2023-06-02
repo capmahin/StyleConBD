e68a33f46be1994221c6a1e30f0ec482bb95319b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-03 04:12:47 +0600

    add new classes

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <Link href="/product/1">ProductCard</Link>
   )
 }
 

6f3d9729b6e498f3c2bbf16aa6bac53ef972c6a8
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-02 20:09:50 +0600

    add

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const HeroBanner = () => {
   return (

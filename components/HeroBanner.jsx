950d118a7050bc5ce97c68f09c67c72fbe3c9f7e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-02 20:24:53 +0600

    add new products

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -8,8 +8,19 @@ const HeroBanner = () => {
   return (
     <div className="relative text-white text-[20px] w-full max-w-[1360px] mx-auto">
         <Carousel>
-            
-        </Carousel>
+                <div>
+                    <img src="/slide-1.png" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="/slide-2.png" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="/slide-3.png" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
     </div>
   )
 }

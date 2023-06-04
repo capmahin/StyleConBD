0bd36befb916cfc910ed1a8669b1313501e3244f
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-04 07:09:47 +0600

    add more optionm

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 
 const ProductCard = () => {
   return (
-    <Link href="/product/1">ProductCard</Link>
+    <Link href="/product/1">
+      <img className="w-full" src="/product-1.webp" alt="" />
+    </Link>
   )
 }
 

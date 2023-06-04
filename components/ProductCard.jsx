dff8cfbdc7659de0bc42093e96b6bedf951d322a
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-04 07:22:41 +0600

    add more section

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -9,6 +9,11 @@ const ProductCard = () => {
       <img className="w-full" src="/product-1.webp" alt="" />
       <div className="p-4 text-black/[0.9]">
       <h2 className="text-lg font-medium">Product Name</h2>
+      <div className="flex items-center text-black/[0.5]">
+      <p className="mr-2 text-lg font-semibold">
+                        $20.00
+                    </p>
+      </div>
       </div>
     </Link>
   )

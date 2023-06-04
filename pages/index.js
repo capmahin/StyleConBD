de1419d64c53a0e11a5a13392241ddb9bb07084b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-04 07:12:20 +0600

    add new components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import HeroBanner from "@/components/HeroBanner"
 import Wrapper from "@/components/Wrapper"
-
+import ProductCard from "@/components/ProductCard"
 
 
 export default function Home() {
@@ -22,7 +22,7 @@ export default function Home() {
                 {/* heading and paragaph end */}
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-
+                  <ProductCard/>
                 </div>
       </Wrapper>
     </main>

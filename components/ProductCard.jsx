7b48c9aa95439989ae2803cf34d7603bce127cdd
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-04 07:20:36 +0600

    add

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -7,6 +7,9 @@ const ProductCard = () => {
   return (
     <Link href="/product/1">
       <img className="w-full" src="/product-1.webp" alt="" />
+      <div className="p-4 text-black/[0.9]">
+      <h2 className="text-lg font-medium">Product Name</h2>
+      </div>
     </Link>
   )
 }

d511e91b812dc8e6aef08a37384a198f4784aebb
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-06 03:58:02 +0600

    use Router hook

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 import Wrapper from "@/components/Wrapper";
 import ProductCard from "@/components/ProductCard";
 const Category = () => {
+  const router = useRouter();
   return (
     <div>Category</div>
   )

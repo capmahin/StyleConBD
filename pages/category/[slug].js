ce16c3525383bc24c1e12082653ca1f98e1c692e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-06 05:44:26 +0600

    add more product pics

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -12,6 +12,18 @@ const Category = () => {
               Running Shoes
             </div>
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+                  <ProductCard/>
+                  <ProductCard/>
+                  <ProductCard/>
+                  <ProductCard/>
+                  <ProductCard/>
+                  <ProductCard/>
+                  <ProductCard/>
+                  <ProductCard/>
+                  <ProductCard/>
+                  
+                </div>
       </Wrapper>
     </div>
   )

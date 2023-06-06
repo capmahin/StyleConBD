9d2ece350fbd8151bd1b71a4abf0ebe17dd9d5f3
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-07 02:28:06 +0600

    add relatedproduct.jsx

---
+++ components/RelatedProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ProductCard from "./ProductCard";
+
+const RelatedProducts = () => {
+  return (
+    <div>RelatedProducts</div>
+  )
+}
+
+export default RelatedProducts

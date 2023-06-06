70facb187dbaa76c86a70d08c2b558a3bfa191eb
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-07 02:57:54 +0600

    add checkout button

--- pages/cart.js
+++ pages/cart.js
@@ -48,7 +48,15 @@ const Cart = () => {
                                         international transaction fees.
                                     </div>
                                 </div>
-
+                             {/* BUTTON START */}
+                             <button
+                                    className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
+                                    
+                                >
+                                    Checkout
+                                    
+                                </button>
+                                {/* BUTTON END */}
                                  </div>
                                  {/* SUMMARY End */}
                         </div>

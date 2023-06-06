61dda9fd1b826b770059570b88121782cfc22027
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-07 02:38:46 +0600

    add new

--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -21,7 +21,8 @@ const RelatedProducts = () => {
   return (
     <div  className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
         <div className="text-2xl font-bold mb-5">You Might Also Like</div>
-        <Carousel responsive={responsive}>
+        <Carousel responsive={responsive}  containerClass="-mx-[10px]"
+                itemClass="px-[10px]">
         <ProductCard/>
         <ProductCard/>
         <ProductCard/>

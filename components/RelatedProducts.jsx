7ff40fbdd1ba7b16cc309ad48abe250885f28b7d
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-07 02:34:25 +0600

    add heading

--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -19,7 +19,9 @@ const RelatedProducts = () => {
         },
     };
   return (
-    <div  className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">RelatedProducts</div>
+    <div  className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
+        <div className="text-2xl font-bold mb-5">You Might Also Like</div>
+    </div>
   )
 }
 

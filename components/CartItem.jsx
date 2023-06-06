287ee10d2dd4eb24fc7ea858fc70e1d9d581b158
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-07 03:13:04 +0600

    add subtille

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+// import Image from "next/image";
+import React from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const CartItem = () => {
   return (
-    <div>CartItem</div>
+    <div className="flex py-5 gap-3 md:gap-5 border-b">
+        {/* IMAGE START */}
+        <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
+                <img src="product-1.webp"  />
+            </div>
+            {/* IMAGE END */}
+
+            <div className="w-full flex flex-col">
+
+                <div className="flex flex-col md:flex-row justify-between">
+                 {/* PRODUCT TITLE */}
+                 <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
+                        Jordan Retro 6 6
+                    </div>
+                </div>
+            </div>
+    </div>
   )
 }
 

2e99d4846f29a7f2dc1791b52d3802e5a795f984
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-06 06:46:14 +0600

    add new div

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,8 +5,16 @@ const ProductDetails = () => {
   return (
     <div className="w-full md:py-20">
         <Wrapper>
-            <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">Left</div>
+            <div>
+             {/* Left column start */}
+             <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">Left</div>
+            {/* Left column end */}
+
+            {/* Right column start */}
             <div className="flex-[1] py-3">Right</div>
+            {/* Right column end */}
+            </div>
+           
         </Wrapper>
     </div>
   )

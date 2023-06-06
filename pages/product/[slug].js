a545ce25e72263d976c6792110de1eb40046a766
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-06 06:50:06 +0600

    add new productdetails crousel

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { IoMdHeartEmpty } from "react-icons/io";
 import Wrapper from "@/components/Wrapper";
+import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
+
 const ProductDetails = () => {
   return (
     <div className="w-full md:py-20">
         <Wrapper>
             <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
              {/* Left column start */}
-             <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">Left</div>
+             <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+                <ProductDetailsCarousel/>
+             </div>
             {/* Left column end */}
 
             {/* Right column start */}

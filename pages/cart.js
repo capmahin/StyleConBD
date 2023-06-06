b08fce6569a4811002da8f303f1591fe4ab7c0bd
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-07 02:42:05 +0600

    import

--- pages/cart.js
+++ pages/cart.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import Wrapper from "@/components/Wrapper";
+import CartItem from "@/components/CartItem";
+import { useSelector } from "react-redux";
 
-const cart = () => {
+const Cart = () => {
   return (
     <div>cart</div>
   )
 }
 
-export default cart
+export default Cart

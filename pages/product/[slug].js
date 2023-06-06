e3376ef00d5fb5552b0811f0dbe5373ddd089185
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-06 06:43:13 +0600

    add new classes in 2 divs

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,8 +5,8 @@ const ProductDetails = () => {
   return (
     <div className="w-full md:py-20">
         <Wrapper>
-            <div>Left</div>
-            <div>Right</div>
+            <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">Left</div>
+            <div className="flex-[1] py-3">Right</div>
         </Wrapper>
     </div>
   )

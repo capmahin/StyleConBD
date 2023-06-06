4feeab2408937fc2a60d52e2b7ec5a045f32bf3b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-07 02:49:51 +0600

    add heading

--- pages/cart.js
+++ pages/cart.js
@@ -2,14 +2,20 @@ import React from 'react'
 import Image from "next/image";
 import Link from "next/link";
 import Wrapper from "@/components/Wrapper";
-import CartItem from "@/components/CartItem";
-import { useSelector } from "react-redux";
+// import CartItem from "@/components/CartItem";
+// import { useSelector } from "react-redux";
 
 const Cart = () => {
   return (
     <div className="w-full md:py-20">
         <Wrapper>
-            
+           {/* HEADING AND PARAGRAPH START */}
+           <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+                            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                                Shopping Cart
+                            </div>
+                        </div>
+                        {/* HEADING AND PARAGRAPH END */}
         </Wrapper>
     </div>
   )

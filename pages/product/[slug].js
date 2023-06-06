19d63f61e6b9f299d285fde04eca60198c1ee2dc
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-06 06:41:58 +0600

    add lft and right section

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,7 +3,12 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import Wrapper from "@/components/Wrapper";
 const ProductDetails = () => {
   return (
-    <div className="w-full md:py-20">ProductDetails</div>
+    <div className="w-full md:py-20">
+        <Wrapper>
+            <div>Left</div>
+            <div>Right</div>
+        </Wrapper>
+    </div>
   )
 }
 
